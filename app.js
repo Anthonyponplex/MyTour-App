71b810565f375837905e609adb2303d1e88f02ad
anthonyponplex <mayakianthony@yahoo.com>
2022-12-19 23:59:37 +0100

    Better errors and refactoring

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./src/utils/appError");
 const tourRouter = require("./src/routes/tourRoutes");
 const userRouter = require("./src/routes/userRoutes");
 
@@ -21,15 +22,14 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/tours/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on the server`,
-  });
+  next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
+  console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
